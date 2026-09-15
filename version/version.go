a4f30f2749dc2ede39002eeeae876b56061ff2fa
jqh <841324345@qq.com>
2026-09-15 17:18:00 +0800

    bump version to 0.3.0

--- version/version.go
+++ version/version.go
@@ -4,7 +4,7 @@ package version
 const Name = "PrismGo"
 
 // Framework is the PrismGo framework version.
-const Framework = "0.2.2"
+const Framework = "0.3.0"
 
 // Banner returns the CLI version banner.
 func Banner() string {
